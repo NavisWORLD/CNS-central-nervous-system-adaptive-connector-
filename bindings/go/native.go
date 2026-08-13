ea80fe3881369c9e1dab66ebd2fd7123022b0c83
NavisWORLD <personal.phera@gmail.com>
2026-08-13 12:28:48 -0500

    Bind Go recurrent core to C ABI

---
+++ bindings/go/native.go
@@ -0,0 +1,37 @@
+package statecore
+
+/*
+#cgo CFLAGS: -I${SRCDIR}/../c/include
+#include "cns_state_core.h"
+*/
+import "C"
+import "unsafe"
+
+func MatrixStep(matrix *[2916]float64, state *[54]float64, gain, rate, decay, limit float64) int {
+	return int(C.cns_weight_update(
+		(*C.double)(unsafe.Pointer(&matrix[0])),
+		(*C.double)(unsafe.Pointer(&state[0])),
+		C.double(gain), C.double(rate), C.double(decay), C.double(limit),
+	))
+}
+
+func MatrixModulation(matrix *[2916]float64, state *[54]float64) ([54]float64, int) {
+	var out [54]float64
+	rc := C.cns_weight_modulation(
+		(*C.double)(unsafe.Pointer(&matrix[0])),
+		(*C.double)(unsafe.Pointer(&state[0])),
+		(*C.double)(unsafe.Pointer(&out[0])),
+	)
+	return out, int(rc)
+}
+
+func Tail(prior, modulation *[54]float64, memoryStrength, entropy float64) ([12]float64, int) {
+	var out [12]float64
+	rc := C.cns_recurrent_tail(
+		(*C.double)(unsafe.Pointer(&prior[0])),
+		(*C.double)(unsafe.Pointer(&modulation[0])),
+		C.double(memoryStrength), C.double(entropy),
+		(*C.double)(unsafe.Pointer(&out[0])),
+	)
+	return out, int(rc)
+}
